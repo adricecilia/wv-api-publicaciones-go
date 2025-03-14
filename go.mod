c0328a18b6593cf3a3e02adec6511b357242e92a
adriancecilia <adrian.cecilia@whitevega.com>
2025-03-14 08:57:01 +0100

    initialize Go module and create main application file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module wv-api-publicaciones-go
+
+go 1.24.1
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Idealista Microservices with Go"
+	fmt.Println("Whitevega", name)
+}
